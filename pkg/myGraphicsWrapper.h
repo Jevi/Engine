16fc1c60847b950b6dacb144385686691ed85352
Jevi <arnunezm@yahoo.com>
2013-02-13 11:22:33 -0600

    removing files

--- pkg/myGraphicsWrapper.h
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: myGraphics
-** Generated automatically by tolua++-1.0.92 on 02/11/13 17:46:55.
-*/
-
-/* Exported function */
-TOLUA_API int  tolua_myGraphics_open (lua_State* tolua_S);
-
